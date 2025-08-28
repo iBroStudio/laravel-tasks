37981eb9259f8ef43efe00b9217ffde3f6c6d9ff
Yann FREPPEL <yann@ibro.studio>
2025-08-28 10:34:46 +0200

    improve addprocessable for process tasks and sub processes

--- src/Concerns/HasProcessableModel.php
+++ src/Concerns/HasProcessableModel.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace IBroStudio\Tasks\Concerns;
 
+use IBroStudio\Tasks\Models\Task;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
@@ -20,6 +21,13 @@ trait HasProcessableModel
 
     public function addProcessable(Model $processable): bool
     {
+        if (method_exists($this, 'tasks')) {
+            $this->tasks->each(function (Task $task) use ($processable) {
+                $task->addProcessable($processable);
+                $task->asProcess?->addProcessable($processable);
+            });
+        }
+
         return $this->processable()
             ->associate($processable)
             ->save();
